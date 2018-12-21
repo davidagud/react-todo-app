7956b94aed72b54e4d2aa40ef9799910671b6df9
David Agudelo <davidagud@gmail.com>
2018-12-21 10:11:53 -0600

    Added todo checkbox function

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,34 @@
+class Todo extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = { done: (this.props.done == "true" && props.done),
+                   text: props.text };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+
+  toggleState(){
+    this.state.done
+  }
+
+  handleClick(event){
+    this.setState({done: !this.state.done
+    })
+  }
+
+  render(){
+
+    return (
+           <div className="todo">
+            <span>
+              <input type="checkbox" checked={this.state.done} onClick={this.handleClick}/>
+              <input type="text" value={this.state.text}/>
+            </span>
+           </div>
+         );
+  }
+}
+
 ReactDOM.render(
-  <h1>Hello, world</h1>,
-  document.getElementById('root')
-);
+  <Todo text="Todo 1" done="true" />, document.getElementById('root'));
